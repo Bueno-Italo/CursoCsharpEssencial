1ab9ae555d0f0f16a3a8557e5b60fd2c3f64b6ee
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-09 08:23:48 -0300

    TiposDeDados

---
+++ TiposDeDados4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Definindo variáveis do tipo string e object e dynamic");
+Console.WriteLine();
+
+string nome = "Curso C#";
+string titulo = "Curso C# Essencial";
+
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+Console.ReadLine();
+
+object nota = 10;
+object valor1 =8.55;
+object nome1 ="Maria";
+object ativa = true;
+object letra = "A";
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(true);
+Console.WriteLine(letra);
+
+Console.ReadLine();
+
+
