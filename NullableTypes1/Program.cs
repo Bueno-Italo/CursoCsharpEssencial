8e1c707fc20f019da99c3f2cf4e2bffb3bb66702
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-13 15:44:55 -0300

    Nullable Types

---
+++ NullableTypes1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## Nullable Types ##\n");
+
+int? i = null;
+double? d = null;
+bool? z = null;
+
+Console.WriteLine(i);
+Console.WriteLine(d);
+Console.WriteLine(z);
+
+Console.ReadLine();
+
+int? a = null;
+
+if (a.HasValue)
+{
+    Console.WriteLine($"a = {a.Value}");
+}
+else
+{
+    Console.WriteLine("a não possui um valor ");
+}
+
+Console.ReadLine();
