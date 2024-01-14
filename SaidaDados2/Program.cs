6d7e62ee074f4a5c052253d14b45e9c7c8d4116a
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-14 18:18:44 -0300

    ImprimindoDados2

---
+++ SaidaDados2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Saida de dados: Usando sequencia de scapes\n");
+
+string local = "c: \\dados\\poesias.txt";
+string frase = "Ele falou: \"Não fui eu\"";
+
+string pizza = "\nPizza\nde\nMussarela";
+string bolo = "\nBolo\tde\tchocolate";
+
+Console.WriteLine(pizza);
+Console.WriteLine(bolo);
+
+Console.ReadKey();
