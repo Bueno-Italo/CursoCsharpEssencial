447951704dbbea0f654abb9a5e31beb263eff0d1
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-16 22:26:37 -0300

    EntradaDeDados

---
+++ EntradaDeDados/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine(" \n Entrada de dados ##!");
+
+Console.WriteLine("\n Informe seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("\n Informe sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nO seu nome é: {nome}");
+Console.WriteLine($"\ne você tem {idade} anos");
