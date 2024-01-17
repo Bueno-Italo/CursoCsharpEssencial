b4c86c8d23951df664f7bb5f0d13603741ba1a42
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-17 08:23:37 -0300

    OperadoresAtirmeticos

---
+++ OperadoresAritmeticos/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("\n ## Operadores Aritiméticos ##");
+
+Console.WriteLine("Infomrme o valor de x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Infomrme o valor de y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nRaiz quadrada de x = {Math.Sqrt(x)}");
+Console.WriteLine($"\nPotência de x elevado a y = {Math.Pow(x,y)}");
+Console.WriteLine($"\nValor mínimo entre x e y = {Math.Min(x,y)}");
+Console.WriteLine($"\nValor máximo entre x e y = {Math.Max(x,y)}");
+
