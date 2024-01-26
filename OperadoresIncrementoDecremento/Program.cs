82d704d6120d772a76686e63432dfe5c90774438
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-26 17:53:52 -0300

    IncrementoDecremento

---
+++ OperadoresIncrementoDecremento/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine(" ## Operadores Incremento e Decremento ## \n");
+
+int x = 0;
+//Imprimindo com interpolação de string
+Console.WriteLine($" x = {x}");
+
+//Pós-decremento -> primeiro resolve e depois decrementa
+int resultado1 = x-- + 10;
+
+Console.WriteLine($"pós-decremento == {resultado1}");
+Console.WriteLine($"valor de x == {x}\n");
+
+//Pré-incremento -> primeiro decrementa e depois resolve
+int y = 0;
+Console.WriteLine($"y = {y}") /
+int resultado1 = --y + 10;
+
+Console.WriteLine($"pré-decremento == {resultado1}");
+Console.WriteLine($"valor de y == {y}\n");
+
+Console.ReadKey();
