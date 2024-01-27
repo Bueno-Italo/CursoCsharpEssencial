37db0911d812272f1a5198cbcc928bf3d5852e3f
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-27 12:20:19 -0300

    OperadorTernario

---
+++ OperadorTernario/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(" ## Operador Ternário $$\n");
+//O operador ternário utiliza 3 argumentos e avalia uma expressão booleana "?:"
+//sintaxe --> condição? expressão1_se_true: expressão2_se_false
+//Condição : qualquer expressão booleana
+//Expressão1_se_true: expressão avaliada sea condição for verdadeira
+//Expressão2_se_false: expressão avaliada se a condição for falsa
+
+Console.WriteLine("Informe a temperatura: \n");
+
+var temp = Convert.ToDouble(Console.ReadLine());
+var resultado = temp > 27 ? "Quente" : "Normal";
+
+Console.WriteLine($"O tempo está {resultado}");
+
+Console.ReadKey();
