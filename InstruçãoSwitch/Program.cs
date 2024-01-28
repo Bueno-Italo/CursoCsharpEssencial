e51dcbb31452c57424da821496ea7dee6989a821
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-28 17:33:38 -0300

    instruçãoSwitch

---
+++ InstruçãoSwitch/Program.cs
@@ -0,0 +1,26 @@
+﻿//O bloco switch é uma estrutura de condição que define o código a ser executado com base em uma comparação de valores
+//A instrução switch pode ser usada para substituir a instrução if-else-if.
+Console.WriteLine(" ## Estrutura Switch ## \n");
+int compra = 600;
+Console.WriteLine("Valor da compra R$ 600,00");
+Console.WriteLine("Informe o numero de parcelas (1 a 3)");
+var numeroParcelas = Convert.ToInt32(Console.ReadLine());
+
+switch (numeroParcelas)
+{
+    case 1: Console.WriteLine($"\nPrestação R$ {compra/numeroParcelas}"); 
+        break;
+
+    case 2: Console.WriteLine($"\nPrestações R$ {compra / numeroParcelas}");
+        break;
+    
+    case 3: Console.WriteLine($"\nPrestações R$ {compra / numeroParcelas}");
+        break;
+
+    default: Console.WriteLine("\nValor invalido, informe 1, 2 ou 3"); 
+        break;
+}
+
+Console.WriteLine("Fim do processamento...");
+
+
