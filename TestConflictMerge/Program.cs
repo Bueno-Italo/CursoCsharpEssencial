60dc92a39575ed10bc34f3a84e0a27f16f975f67
italo <italo-bueno@hotmail.com>
2024-02-07 22:41:10 -0300

    Resolvendo Conflitos de merge

--- TestConflictMerge/Program.cs
+++ TestConflictMerge/Program.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("Resolve conflict teste");
+﻿using System.Globalization;
+
+Console.WriteLine("Resolve conflict teste");
 Console.ReadKey();
+
+Console.WriteLine("Resolvendo conflito v2");
+//Teste para solucionar conflito de merge relacionado ao push dos pull request criados 
