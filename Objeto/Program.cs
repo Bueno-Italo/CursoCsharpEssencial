ad7af5bfb63c306c41cb916fbe8ec9834fe3c35d
italo <italo-bueno@hotmail.com>
2024-02-11 15:40:56 -0300

    Objeto

---
+++ Objeto/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Objeto é uma instancia de uma classe, criado pelo operador new, e, todos os membros
+da classe podem ser acessador através do objeto*/
+
+Console.WriteLine("##\nObjeto##");
+
+Pessoa p1 = new();
+p1.nome = "Maria";
+p1.idade = 25;
+p1.sexo = "Feminino";
+
+Console.WriteLine($"{p1.nome} {p1.idade} {p1.sexo}");
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? nome;
+    public int idade;
+    public string sexo;
+}
